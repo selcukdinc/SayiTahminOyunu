cc120b2ebbb7742e67d4532863aac5f6ca94ae40
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-03-15 12:55:24 +0300

    versiyon 1.01

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // TAHMiN OYUNU
+        //TAHMiN OYUNU
         // OYUNUN AMACI
         // Uygulama olarak bu örnekte yapacağımız basit bir tahmin oyununun genel olarak amacı ve yapacağı işlemler aşağıdaki gibidir.
-        // 
+         
         // • Program, açıldığında 1 ile 1000 arasında bir sayı üretsin ve bu sayı "n" değişkenine atansın.
         // • Kullanıcının 11 hakkı vardır. Bu nedenle hak adında bir değişken tanımlansın ve ilk değeri 11 yapılsın.
-        // • Bir döngü açılsın ve "sayi" adında tanımlanmış bir değişkenin değeri klavyeden okunsun. Eğer "n" sayısı girilen sayıdan büyük ise kullanıcıya "Sayıyı Yükset"
-        // şeklinde bir yazı, küçük ise "Sayıyı Küçült" şeklinde bir yazı gösterilsin ve sayı bilinemediği için hak değişkeninin değeri bir küçültülsün. Ancak girilen sayı,
+        // • Bir döngü açılsın ve "sayi" adında tanımlanmış bir değişkenin değeri klavyeden okunsun.Eğer "n" sayısı girilen sayıdan büyük ise kullanıcıya "Sayıyı Yükset"
+        // şeklinde bir yazı, küçük ise "Sayıyı Küçült" şeklinde bir yazı gösterilsin ve sayı bilinemediği için hak değişkeninin değeri bir küçültülsün.Ancak girilen sayı,
         // üretilmiş olan n sayısına eşit ise ekrana "Tebrikler" şeklinde bir yazı gösteriir ve oyun bitirilsin.
         // • Eğer kullanıcı sahip olduğu 11 hak ile "n" sayısını tahmin edemediyse ekrana
         // "Kaybettin" şeklinde bir yazı gösterilsin ve oyun bitirilsin.
